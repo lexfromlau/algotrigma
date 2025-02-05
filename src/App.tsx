e37a0d652763166505af356bb7c3e47649759771
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2025-02-05 23:10:05 +0100

    initial fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { fetchData } from "./util/fetchData";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const handleFetchData = () => {
+    fetchData(
+      `https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    )
+      .then((data) => {
+        const dailyTimeSeries = data["Time Series FX (Daily)"];
+        console.log({ dailyTimeSeries });
+        console.log({ data });
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Fetch data:</div>
+      <button onClick={() => handleFetchData()}>fetch</button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

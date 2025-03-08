6cf3ee3c726734e638c846e579e5c4a18f79ccba
alex <alexander.polzhofer@gmail.com>
2025-03-08 14:43:15 +0100

    use generics for fetchData method, implement ticker search endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
 import React from "react";
 import { fetchData } from "./util/fetchData";
 
+interface TickerSearchData {
+  bestMatches: {
+    "1. symbol": string;
+    "2. name": string;
+    "3. type": string;
+    "4. region": string;
+    "5. marketOpen": string;
+    "6. marketClose": string;
+    "7. timezone": string;
+    "8. currency": string;
+    "9. matchScore": string;
+  }[];
+}
+
 const App = () => {
-  const handleFetchData = () => {
-    fetchData(
-      `https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=${
-        import.meta.env.VITE_API_KEY
-      }`
-    )
-      .then((data) => {
-        const dailyTimeSeries = data["Time Series FX (Daily)"];
-        console.log({ dailyTimeSeries });
-        console.log({ data });
-      })
-      .catch((error) => console.error(error));
+  const [tickerData, setTickerData] = React.useState<TickerSearchData>();
+
+  const sendSearchDebounced = (searchValue: string) => {
+    const url = `${
+      import.meta.env.VITE_VANTAGE_BASE_URL
+    }/query?function=SYMBOL_SEARCH&keywords=${searchValue}&apikey=${
+      import.meta.env.VITE_API_KEY
+    }`;
+
+    setTimeout(async () => {
+      const fetchResult = await fetchData<TickerSearchData>(url);
+      setTickerData(fetchResult);
+    }, 500);
   };
 
   return (
     <>
       <div>Fetch data:</div>
-      <button onClick={() => handleFetchData()}>fetch</button>
+      <label htmlFor="ticker-search" />
+      <input
+        id="ticker-search"
+        type="text"
+        onChange={(e) =>
+          e.target.value.length >= 3 && sendSearchDebounced(e.target.value)
+        }
+      />
+      {tickerData?.bestMatches !== undefined &&
+        tickerData.bestMatches.length > 0 &&
+        tickerData.bestMatches.map((dataObj) => (
+          <p>{`${dataObj["1. symbol"]} - ${dataObj["2. name"]}`}</p>
+        ))}
     </>
   );
 };
--- src/util/fetchData.ts
+++ src/util/fetchData.ts
@@ -1,10 +1,13 @@
-export async function fetchData(url: string | URL, options: RequestInit = {}) {
+export async function fetchData<T>(
+  url: string | URL,
+  options: RequestInit = {}
+): Promise<T> {
   try {
     const response = await fetch(url, options);
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    const data = await response.json();
+    const data: T = await response.json();
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
